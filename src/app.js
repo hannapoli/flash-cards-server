3a23d755252794668c8012b2e4c4ddcd37ed1661
HannaPolishchuk <hanna.polishchuk@gmail.com>
2025-12-21 15:43:46 +0100

    ADD: created a basic structure for the project.

---
+++ src/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const frontendUrl = '';
+const frontendLocalhost = '';
+const whitelist = [frontendUrl, frontendLocalhost];
+const corsOptions = {
+    origin: (origin, callback) => {
+        //!origin para permitir las peticiones del Postman
+        if (whitelist.includes(origin) || !origin) {
+            //callback(error, allow)
+            callback(null, true);
+        } else {
+            console.log('Origen no permitido por CORS:', origin);
+            callback(new Error('Esta conexión no está permitida por CORS'));
+        }
+    },
+    optionsSuccessStatus: 200,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
+
+// ============= MIDDLEWARES =============
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(cors(corsOptions));
+
+// ============= EJS para la página principal: =============
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// ============= RUTAS =============
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.use('/api/v1', require('./routes/users.routes'));
+//languages, categories, words, collections, uploads, auth (FB?)
+
+app.listen(port, () => {
+    console.log(`Servidor activo en el puerto ${port} 📡​`);
+});
---
+++ tables.sql
@@ -0,0 +1,17 @@
+DROP TABLE collectios;
+DROP TABLE words;
+DROP TABLE categies;
+DROP TABLE languages;
+DROP TABLE users;
+-- DROP TABLE uploads;
+
+CREATE TABLE users ();
+CREATE TABLE languages ();
+CREATE TABLE categories ();
+CREATE TABLE words ();
+CREATE TABLE collections ();
+-- CREATE TABLE uploads ();
+
+INSERT INTO users ()
+VALUES
+();
