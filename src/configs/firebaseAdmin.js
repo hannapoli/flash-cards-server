d28abb3876dbc9e57b5de43686e1b95cc4375f8d
HannaPolishchuk <hanna.polishchuk@gmail.com>
2026-01-10 21:01:49 +0100

    UPDATE: Firebase configuration for deployment.

--- src/configs/firebaseAdmin.js
+++ src/configs/firebaseAdmin.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccount = require(`${process.env.SERVICE_ACCOUNT}`);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
